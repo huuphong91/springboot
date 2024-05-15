113838172a0ad8deff350536fc4d06f060166575
Huu <phong.huu@clpsglobal.com>
2024-05-15 16:27:00 +0700

    test

---
+++ src/main/java/clps/hibernate/demoonetoone/DemoOneToOneApplication.java
@@ -0,0 +1,13 @@
+package clps.hibernate.demoonetoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOneToOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOneToOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/clps/hibernate/demoonetoone/DemoOneToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package clps.hibernate.demoonetoone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOneToOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
